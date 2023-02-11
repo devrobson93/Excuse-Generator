4280fb09a9fbc35ca21a819e9a15e9da6362fa1c
devrobson93 <robson.dev93@gmail.com>
2023-02-11 21:02:46 +0000

    primeros pasos en javascript

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generarRandom(arrayLenght) {
+  return Math.floor(Math.random() * arrayLenght);
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getWho() {
+  const who = ["Maggie", "Bart", "Lisa", "Marge", "Ned Flanders"];
+  return who[generarRandom(who.length)];
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function getAction() {
+  const action = ["comió", "meó", "aplastó", "rompió"];
+  return action[generarRandom(action.length)];
+}
+
+function getWhat() {
+  const what = [
+    "mis deberes",
+    "las llaves",
+    "el coche",
+    "la comida",
+    "el libro"
+  ];
+  return what[generarRandom(what.length)];
+}
+
+function getWhen() {
+  const when = [
+    "antes de clase",
+    "justo ahora",
+    "cuando he acabado",
+    "durante mi almuerzo",
+    "encuanto estaba rezando"
+  ];
+  return when[generarRandom(when.length)];
+}
+
+function emitirAlert(respuesta) {
+  // utilizo join para mezclar las palabras y cambiar la coma
+  // por el caracter espacio (puede utilizar lo que quieras)
+  document.getElementById("respuesta").innerHTML = respuesta.join(" ");
+}
+
+function generarExcusa() {
+  let respuesta = [];
+  respuesta.push(getWho());
+  respuesta.push(getAction());
+  respuesta.push(getWhat());
+  respuesta.push(getWhen());
+
+  emitirAlert(respuesta);
+}
